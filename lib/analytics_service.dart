76622a148eb162e27f7d0d8f3c433f457d7833f5
James Schafer <awsjs@amazon.com>
2021-03-05 18:57:47 +1100

    fix up local code to match Workshop

--- lib/analytics_service.dart
+++
@@ -1,8 +0,0 @@
-import 'analytics_events.dart';
-import 'package:amplify_flutter/amplify.dart';
-
-class AnalyticsService {
-  static void log(AbstractAnalyticsEvent event) {
-    Amplify.Analytics.recordEvent(event: event.value);
-  }
-}
